1b54b155dfc2fb31a9f48cee42ae3186e6b96b23
Erika Yesid <erika00yesid@gmail.com>
2026-05-21 16:56:29 -0500

    User data: creación de variables

---
+++ src/user_data.py
@@ -0,0 +1,17 @@
+# Simulador de nomina
+
+nombre = "Erika"
+salario = 1.750, 905
+horas_extras = 10
+auxilio_transporte = 249.095
+tipo_contrato = "Indefinido"
+remoto = False
+
+print("Nombre:", nombre)
+print("Salario:", salario)
+print("Horas extras:", horas_extras)
+print("Auxilio de transporte:", auxilio_transporte)
+print("Tipo de contrato:", tipo_contrato)
+print("¿Trabaja de forma remota?", remoto)
+
+print(type(nombre))
